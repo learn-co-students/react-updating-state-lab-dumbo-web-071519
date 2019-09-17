98f5ca29901b09aa060a9d410a9761d8f5cf15f5
jason <jasonedlewis@gmail.com>
2019-09-17 19:14:04 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+export default class DigitalClicker extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            timesClicked: 0
+        }
+    }
+    addClick =()=>{
+        this.setState({
+            timesClicked: this.state.timesClicked + 1
+        })
+    }
+
+    render( ){
+        return (
+            <button value="Times Clicked" onClick={this.addClick}>Times Clicked  {this.state.timesClicked}</button>
+
+        )
+    }
+
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,14 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component{
+    constructor(){
+        super()
+    }
+    render( ){
+        return (
+            <div></div>
+
+        )
+    }
+}
